c8ff51d78155fc47e0245aa307dbd6a5063908d5
Udhayan K <kudhayan27@gmail.com>
2026-01-08 23:53:07 +0530

    Added Struct for request and response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize,Serialize};
+use std::sync::Arc;
+
+#[derive(Clone)]
+pub struct User{
+    pub uid: String,
+    pub email:String,
+    pub pw:String,
+    pub role: String,
+}       
+
+#[derive(Deserialize)]
+pub struct LoginRequest{
+    pub email:String,
+    pub pw: String,
+}
+
+#[derive(Serialize)]
+pub struct LoginResponse{
+    pub token:String,
+}
+
+#[tokio::main]
+async fn main(){
+    let user = Arc::new(intit_users());
 }
